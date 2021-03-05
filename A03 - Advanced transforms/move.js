3a5daae1d6e680db2722d93a56eacc4ba7dd343d
lolepls <gioele.mombelli@mail.polimi.it>
2021-03-05 11:35:31 +0100

    Complete 3/4 assignment 03

--- A03 - Advanced transforms/move.js
+++ A03 - Advanced transforms/move.js
@@ -2,7 +2,7 @@
 This third CG assignment is about transformations around arbitrary axes, inverse of transformations and composition of transformations.
 The chosen approach is to write a separate function for each transformation that returns the correct matrix. Some of these functions will
 perform computations in a more explicit way for learning purposes, others will exploit the function library "utils.js" to avoid writing 
-the matrixes explicitly.
+the matrices explicitly.
 */
 
 function computeR1Matrix(){
@@ -61,7 +61,7 @@ function computeR1Matrix(){
 	  Now we compose the trasformations according to the matrix-on-the-left convention:
 	  p' = T*Ry*Rz * Rx * iRz*iRy*iT * p
 	  We will return a matrix M which is the composition of those transformations. The multiplication of the 
-	  matrixes will be done by exploiting a library function. We only have to take care of the order of the operations.
+	  matrices will be done by exploiting a library function. We only have to take care of the order of the operations.
 	*/  
 
 	var M = utils.multiplyMatrices(T, Ry);
@@ -79,7 +79,8 @@ function computeS1Matrix(){
 
 	/*
   	In order to perform requested scaling we have to align the x axis to the bisector of the xy positive plane. This can be
-  	obtained with a rotation of +45 degrees around the z-axis.
+  	obtained with a rotation of +45 degrees around the z-axis. We then perform the scaling of 0.5 on the x axis and
+        we rotate back.
 	*/
 
 	var gamma = utils.degToRad(45);
@@ -115,6 +116,55 @@ function computeS1Matrix(){
 
 }
 
+function computeS2Matrix(){
+// Mirror the starship along a plane passing through (1,1,1), and obtained rotating 15 degree around the x axis the xz plane.
+
+	/*
+  	In order to perform requested mirroring we have to perform a traslation on (1,1,1) and a rotation of 15 degrees 
+	around the x axis. Since the plane of the mirroring is xz, the operation will need Sy = -1.
+	*/
+
+	//This is the matrix for the translation. Both T and its inverse will be needed.
+	var T  =          [1.0,		0.0,		0.0,		1.0,
+			   0.0,		1.0,		0.0,		1.0,
+			   0.0,		0.0,		1.0,	        1.0,
+			   0.0,		0.0,		0.0,		1.0];
+	var iT = utils.invertMatrix(T);
+	
+	//This is the matrix for the rotation around the x axis.
+	var alfa = utils.degToRad(15);
+	var cosalfa = Math.cos(alfa);
+	var senalfa = Math.sin(alfa);
+
+
+	var Rx  =         [1.0,		0.0,		0.0,		0.0,
+			   0.0,		cosalfa,	-senalfa,	0.0,
+			   0.0,		senalfa,	cosalfa,	0.0,
+			   0.0,		0.0,		0.0,		1.0];
+	var iRx = utils.invertMatrix(Rx);
+	
+
+	//The mirroring w.r.t. the plane xz is simply a matrix with s = -1 on the y axis:
+
+	var Sy =          [1.0,		0.0,		0.0,		0.0,
+			   0.0,		-1.0,		0.0,		0.0,
+			   0.0,		0.0,		1.0,		0.0,
+			   0.0,		0.0,		0.0,		1.0];
+
+	/*
+	As usual, this is the composition of the matrices in order to obtain M, which represents the following operations:
+	p' = M * p
+	M = T*Rx * Sy * iRx*iT
+	*/
+
+	var M = utils.multiplyMatrices(T, Rx);
+	M = utils.multiplyMatrices(M, Sy);
+	M = utils.multiplyMatrices(M, iRx);
+	M = utils.multiplyMatrices(M, iT);
+	return M;
+
+}
+
 function move() {
 	// Rotate 60 degrees around an arbitrary axis passing through (0,1,-1). The x-axis can be aligned to the arbitrary axis after a rotation of 45 degrees around the z-axis, and then 15 degrees around the y-axis.
 	var R1 = computeR1Matrix();
@@ -123,10 +173,7 @@ function move() {
 	var S1 = computeS1Matrix();
 			   
 	// Mirror the starship along a plane passing through (1,1,1), and obtained rotating 15 degree around the x axis the xz plane
-	var S2 =  [1.0,		0.0,		0.0,		0.0,
-			   0.0,		1.0,		0.0,		0.0,
-			   0.0,		0.0,		1.0,		0.0,
-			   0.0,		0.0,		0.0,		1.0];
+	var S2 = computeS2Matrix();
 			   
 	// Apply the inverse of the following sequence of transforms: rotation of 30 degree around the Y axis then Translation of (0, 0, 5), and finally a uniform scaling of a factor of 3.
 	var I1 =  [1.0,		0.0,		0.0,		0.0,
